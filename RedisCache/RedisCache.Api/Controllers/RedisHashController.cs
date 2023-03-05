97145ec91cd0b040429608cf54fb514328e2720b
shako <shalva.turashvili.1@btu.edu.ge>
2023-03-05 19:33:33 +0400

    RedisHash and RedisSet examples

---
+++ RedisCache/RedisCache.Api/Controllers/RedisHashController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisCache.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisHashController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _connectionMultiplexer;
+        private const string HashKey = "MaHash";
+
+        public RedisHashController(IConnectionMultiplexer connectionMultiplexer)
+        {
+            _connectionMultiplexer = connectionMultiplexer;
+        }
+
+        [HttpPost("SetHash")]
+        public async Task SetHash(Dictionary<string, string> values) // Never do this in actual code
+        {
+            var db = _connectionMultiplexer.GetDatabase();
+            foreach (var keyValue in values)
+            {
+                await db.HashSetAsync(HashKey, new HashEntry[] { new(keyValue.Key, keyValue.Value) });
+            }
+        }
+
+        [HttpPost("SetNewKey")]
+        public async Task SetNewKey(string key, Dictionary<string, string> values)
+        {
+            var db = _connectionMultiplexer.GetDatabase();
+            foreach (var keyValue in values)
+            {
+                await db.HashSetAsync(key, new HashEntry[] { new(keyValue.Key, keyValue.Value) });
+            }
+        }
+
+        [HttpGet("GetHash")]
+        public async Task<Dictionary<string, string>> GetData(string key)
+        {
+            var db = _connectionMultiplexer.GetDatabase();
+            var value = await db.HashGetAllAsync(key);
+            var toReturn = value.ToDictionary<HashEntry, string, string>(hashEntry => hashEntry.Name, hashEntry => hashEntry.Value);
+            return toReturn;
+        }
+    }
+}
---
+++ RedisCache/RedisCache.Api/Controllers/RedisSetsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace RedisCache.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisSetsController : ControllerBase
+    {
+        private static readonly ConnectionMultiplexer Redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                EndPoints =
+                {
+                    "localhost:32768"
+                },
+                Password = "redispw"
+            });
+
+        private const string SetKey = "MaSet";
+
+        [HttpPost("Add")]
+        public async Task AddSet(string value)
+        {
+            var test = ConnectionMultiplexer.Connect("localhost:32768;password=redispwd");
+            var db = Redis.GetDatabase();
+            await db.SetAddAsync(SetKey, new RedisValue[] { value });
+        }
+
+        [HttpGet("GetAll")]
+        public string[] GetSet()
+        {
+            var db = Redis.GetDatabase();
+            var values = db.SetMembers(SetKey);
+            return values.Select(x => x.ToString()).ToArray();
+        }
+
+        [HttpGet("GetOne")]
+        public string GetOneFromSet(string key)
+        {
+            var db = Redis.GetDatabase();
+            var value = db.SetMembers(SetKey);
+            var check = value.First(x => x.ToString().Split(":")[1] == key); // TODO don't like this one
+
+            return check.ToString();
+        }
+    }
+}
