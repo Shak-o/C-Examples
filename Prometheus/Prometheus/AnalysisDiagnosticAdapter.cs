4b36a4a65358f81b116c194645980227e832ad11
Shako <shalva.turashvili.1@btu.edu.ge>
2023-07-16 21:35:39 +0400

    small updates

--- Prometheus/Prometheus/AnalysisDiagnosticAdapter.cs
+++ Prometheus/Prometheus/AnalysisDiagnosticAdapter.cs
@@ -14,13 +14,13 @@ namespace Prometheus
         [DiagnosticName("Microsoft.AspNetCore.MiddlewareAnalysis.MiddlewareException")]
         public void OnMiddlewareException(Exception exception, HttpContext httpContext, string name, Guid instance, long timestamp, long duration)
         {
-            _reporter.RegisterMiddlewareExecutionTime(name, "fail", duration);
+            _reporter.RegisterMiddlewareExecutionTime(name, "fail", TimeSpan.FromTicks(duration).Milliseconds);
         }
 
         [DiagnosticName("Microsoft.AspNetCore.MiddlewareAnalysis.MiddlewareFinished")]
         public void OnMiddlewareFinished(HttpContext httpContext, string name, Guid instance, long timestamp, long duration)
         {
-            _reporter.RegisterMiddlewareExecutionTime(name, "finished", duration);
+            _reporter.RegisterMiddlewareExecutionTime(name, "finished", TimeSpan.FromTicks(duration).Milliseconds);
         }
     }
 }
