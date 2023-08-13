248d24e5b4fc8958fedf2c6cb5d6996e766a6efd
shako <shalva.turashvili.1@btu .edu.ge>
2023-08-13 21:02:25 +0400

    migrations

---
+++ CQRS/query/MyQueryApp.Persistence/Migrations/20230813164451_UpdatedPersonQueryModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyQueryApp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedPersonQueryModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IdInMainTable",
+                table: "PersonQueryModels",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdInMainTable",
+                table: "PersonQueryModels");
+        }
+    }
+}
---
+++ CQRS/query/MyQueryApp.Persistence/Migrations/20230813164547_IndexUptade.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyQueryApp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndexUptade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersonQueryModels_Id_Name_SurName",
+                table: "PersonQueryModels");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonQueryModels_Id_Name_SurName_IdInMainTable",
+                table: "PersonQueryModels",
+                columns: new[] { "Id", "Name", "SurName", "IdInMainTable" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PersonQueryModels_Id_Name_SurName_IdInMainTable",
+                table: "PersonQueryModels");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonQueryModels_Id_Name_SurName",
+                table: "PersonQueryModels",
+                columns: new[] { "Id", "Name", "SurName" });
+        }
+    }
+}
