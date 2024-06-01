c369a5ccc79cb11b3ee07b601dfbce17d9b9e81b
Shak-o <shalva.turashvili.1@btu.edu.ge>
2024-06-01 19:48:51 +0400

    ups

--- SignalR/SignalRDemo/src/SignalRDemo.AppHost/Program.cs
+++ SignalR/SignalRDemo/src/SignalRDemo.AppHost/Program.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var redis = builder.AddRedis("redis");
 var api = builder.AddProject<Projects.SignalRDemo_Api>("api")
+    .WithReference(redis)
     .WithReplicas(2);
 
 builder.AddProject<Projects.SignalRDemo_Client>("client")
